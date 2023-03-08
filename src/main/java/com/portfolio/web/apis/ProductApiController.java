1577d0017cef467ba99185172a907e58bd26b8ea
Jaesuk <jaesuk95@outlook.com>
2023-03-08 21:50:21 +0900

    product api 시작

--- src/main/java/com/portfolio/web/apis/ProductApiController.java
+++ src/main/java/com/portfolio/web/apis/ProductApiController.java
@@ -1,16 +1,27 @@
 package com.portfolio.web.apis;
 
+import com.portfolio.domain.common.ProductSearchCommand;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequiredArgsConstructor
 public class ProductApiController extends AbstractBaseController{
 
+
+
     @GetMapping("/api/product")
-    public ResponseEntity.BodyBuilder get() {
+    public ResponseEntity.BodyBuilder getProducts(
+            Pageable pageable, HttpServletRequest request) {
+        ProductSearchCommand command = ProductSearchCommand.builder()
+                .pageable(pageable)
+                .build();
+        addTriggeredBy(command,request);
         return ResponseEntity.ok();
     }
 }
