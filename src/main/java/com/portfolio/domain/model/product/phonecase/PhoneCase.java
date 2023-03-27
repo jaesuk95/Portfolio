286db164f7411f8baa8d64f24342a7250a04bdd0
Jaesuk <jaesuk95@outlook.com>
2023-03-27 19:37:03 +0900

    phoneCase DTO

--- src/main/java/com/portfolio/domain/model/product/phonecase/PhoneCase.java
+++ src/main/java/com/portfolio/domain/model/product/phonecase/PhoneCase.java
@@ -5,9 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.DiscriminatorValue;
-import javax.persistence.Entity;
-import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.*;
 
 @Getter
 @Entity
@@ -17,6 +15,7 @@ import javax.persistence.PrimaryKeyJoinColumn;
 public class PhoneCase extends Product {
 
     // db 에 phoneType 을 각자 저장하는 이유: 모델마다 가격이 다르거나, 이름이 다를수 있기 때문에
+    @Enumerated(EnumType.STRING)
     private PhoneType phoneType;
     private boolean sale;
 
--- src/main/java/com/portfolio/domain/model/product/phonecase/PhoneCaseData.java
+++ src/main/java/com/portfolio/domain/model/product/phonecase/PhoneCaseData.java
@@ -1,4 +1,18 @@
 package com.portfolio.domain.model.product.phonecase;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+@Getter
+@Setter
 public class PhoneCaseData {
+    private Long id;
+    @Enumerated(EnumType.STRING)
+    private PhoneType phoneType;
+    private String name;
+    private int price;
+    private String modelName;
 }
